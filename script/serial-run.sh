bcbe28fd25ac1f9b22a6a4c2ac4127e1509c53f5
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-04-16 23:36:24 +0200

    Fixed input parameter checks

--- script/serial-run.sh
+++ script/serial-run.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 
-ITERATIONS=1
-TEST="circle"
+iterations=1
+input_name="circle"
 
 if [[ $# == "1" ]] && [[ "$1" =~ ^[0-9]+$ ]]; then
-    ITERATIONS="$1";
-elif [[ $# == "1" ]]; then
-    TEST="$1";
-elif [[ $# == "2" ]] && [[ "$1" =~ ^[0-9]+$ ]]; then
-    ITERATIONS="$1"
-    TEST="$2";
-elif [[ $# == "2" ]] && [[ "$2" =~ ^[0-9]+$ ]]; then
-    ITERATIONS="$2"
-    TEST="$1";
+    iterations="$1";
+elif [[ $# == "1" ]] && [[ `find ./input/$1* | wc -l` == "1" ]]; then
+    input_name="$1";
+elif [[ $# == "2" ]] && [[ "$1" =~ ^[0-9]+$ ]] && [[ `find ./input/$2* | wc -l` == "1" ]]; then
+    iterations="$1"
+    input_name="$2";
+elif [[ $# == "2" ]] && [[ "$2" =~ ^[0-9]+$ ]] && [[ `find ./input/$1* | wc -l` == "1" ]]; then
+    iterations="$2"
+    input_name="$1";
 elif [[ $# != "0" ]]; then
     echo "You can pass the number of iterations and the input file"
-    echo '"./serial-run.sh [N] [inputName]"';
+    echo '"./serial-run.sh [N] [inputName]" or "./serial-run.sh [inputName] [N]"'
+    exit 1;
 fi
 
 time_sum=0
 
-echo "" > ./bench/${TEST}-serial.data
+echo "" > ./bench/${input_name}-serial.data
 
-for (( i="0" ; $i<$ITERATIONS ; i=$i+1 )); do
-    ./bin/omp-skyline < ./input/${TEST}*.in > ./output/${TEST}-serial.out 2>./temp
-    diff ./output/${TEST}-serial.out ./output/serial/${TEST}*.out;
+for (( i="0" ; $i<$iterations ; i=$i+1 )); do
+    ./bin/omp-skyline < ./input/${input_name}*.in > ./output/${input_name}-serial.out 2>./temp
+    diff ./output/${input_name}-serial.out ./output/serial/${input_name}*.out;
 
     time_string=`tail -n 1 ./temp`
-    echo "Iteration #$(($i+1)), ${time_string}" | tee -a ./bench/${TEST}-serial.data
+    echo "Iteration #$(($i+1)), ${time_string}" | tee -a ./bench/${input_name}-serial.data
     time_sum=$(echo ${time_sum} + ${time_string:19} | bc)
 done
 
-echo "Average execution time (s): $(echo "scale=6; ${time_sum} / ${ITERATIONS}" | bc)" | tee -a ./bench/${TEST}-serial.data
+echo "Average execution time (s): $(echo "scale=6; ${time_sum} / ${iterations}" | bc)" | tee -a ./bench/${input_name}-serial.data
 
 rm -f ./temp
