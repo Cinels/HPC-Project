adcf5312e002201d7fb2c639d4634728a9387e04
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-04-22 23:56:19 +0200

    Added mpi launch script

---
+++ script/mpi-run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+iterations=1
+input_name="circle"
+
+if [[ $# == "1" ]] && [[ "$1" =~ ^[0-9]+$ ]]; then
+    iterations="$1";
+elif [[ $# == "1" ]] && [[ `find ./input/$1* | wc -l` == "1" ]]; then
+    input_name="$1";
+elif [[ $# == "2" ]] && [[ "$1" =~ ^[0-9]+$ ]] && [[ `find ./input/$2* | wc -l` == "1" ]]; then
+    iterations="$1"
+    input_name="$2";
+elif [[ $# == "2" ]] && [[ "$2" =~ ^[0-9]+$ ]] && [[ `find ./input/$1* | wc -l` == "1" ]]; then
+    iterations="$2"
+    input_name="$1";
+elif [[ $# != "0" ]]; then
+    echo "You can pass the number of iterations and the input file"
+    echo '"./omp-run.sh [N] [inputName]" or "./omp-run.sh [inputName] [N]"'
+    exit 1;
+fi
+
+time_sum=0
+
+echo "" > ./bench/mpi/${input_name}.data
+
+for (( i="0" ; $i<$iterations ; i=$i+1 )); do
+    mpirun --stdin 0 ./bin/mpi-skyline < ./input/${input_name}*.in > ./output/mpi/${input_name}.out 2>./temp
+    diff ./output/mpi/${input_name}.out ./output/serial/${input_name}*.out;
+
+    time_string=`tail -n 1 ./temp`
+    echo "Iteration #$(($i+1)), ${time_string}" | tee -a ./bench/mpi/${input_name}.data
+    time_sum=$(echo ${time_sum} + ${time_string:19} | bc)
+done
+
+echo "Average execution time (s): $(echo "scale=6; ${time_sum} / ${iterations}" | bc)" | tee -a ./bench/mpi/${input_name}.data
+
+rm -f ./temp
