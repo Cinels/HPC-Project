5f4ebc6ea409aa0f543ac02ec0c94627e21c43f6
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-05-29 11:51:36 +0200

    Updated all-run script to run from 1 to n threads

--- script/all-run.sh
+++ script/all-run.sh
@@ -19,43 +19,45 @@ elif [[ $# != "0" ]]; then
     exit 1;
 fi
 
-serial_time_sum=0
-omp_time_sum=0
-mpi_time_sum=0
+for (( num_threads="1" ; $num_threads<"5" ; num_threads=$num_threads+1 )); do
 
-echo "" > ./bench/serial/${input_name}.data
-echo "" > ./bench/omp/${input_name}.data
-echo "" > ./bench/mpi/${input_name}.data
+	serial_time_sum=0
+	omp_time_sum=0
+	mpi_time_sum=0
 
-echo "                serial     omp       mpi"
-for (( i="0" ; $i<$iterations ; i=$i+1 )); do
-    ./bin/skyline < ./input/${input_name}*.in > ./output/serial/${input_name}.out 2>./.serialtemp
-    ./bin/omp-skyline < ./input/${input_name}*.in > ./output/omp/${input_name}.out 2>./.omptemp
-    mpirun --stdin 0 ./bin/mpi-skyline < ./input/${input_name}*.in > ./output/mpi/${input_name}.out 2>./.mpitemp
+	echo "" > ./bench/omp/${input_name}/p${num_threads}.data
+	echo "" > ./bench/mpi/${input_name}/p${num_threads}.data
 
-    diff ./output/omp/${input_name}.out ./output/serial/${input_name}*.out
-    diff ./output/mpi/${input_name}.out ./output/serial/${input_name}*.out
+	echo "p=$num_threads               omp       mpi"
+	for (( i="0" ; $i<$iterations ; i=$i+1 )); do
+	    OMP_NUM_THREADS=${num_threads} ./bin/omp-skyline < ./input/${input_name}*.in > ./output/serial-omp/${input_name}.out 2>./.omptemp
+	    mpirun --stdin 0 -n ${num_threads} ./bin/mpi-skyline < ./input/${input_name}*.in > ./output/serial-mpi/${input_name}.out 2>./.mpitemp
 
-    serial_time_string=`tail -n 1 ./.serialtemp`
-    omp_time_string=`tail -n 1 ./.omptemp`
-    mpi_time_string=`tail -n 1 ./.mpitemp`
+	    diff ./output/omp/${input_name}.out ./output/serial/${input_name}*.out
+	    diff ./output/mpi/${input_name}.out ./output/serial/${input_name}*.out
 
-    iteration_string="Iteration #$(($i+1)), "
-    echo "${iteration_string}${serial_time_string}" >> ./bench/serial/${input_name}.data
-    echo "${iteration_string}${omp_time_string}" >> ./bench/omp/${input_name}.data
-    echo "${iteration_string}${mpi_time_string}" >> ./bench/mpi/${input_name}.data
-#    echo "${iteration_string}${serial_time_string}" | tee -a ./bench/omp/${input_name}.data
-    echo "${iteration_string}${serial_time_string:19}, ${omp_time_string:19}, ${mpi_time_string:19}"
-    serial_time_sum=$(echo ${serial_time_sum} + ${serial_time_string:19} | bc)
-    omp_time_sum=$(echo ${omp_time_sum} + ${omp_time_string:19} | bc)
-    mpi_time_sum=$(echo ${mpi_time_sum} + ${mpi_time_string:19} | bc)
+	#    serial_time_string=`tail -n 1 ./.serialtemp`
+	    omp_time_string=`tail -n 1 ./.omptemp`
+	    mpi_time_string=`tail -n 1 ./.mpitemp`
+
+	    iteration_string="Iteration #$(($i+1)), "
+	    echo "${iteration_string}${omp_time_string}" >> ./bench/omp/${input_name}/p${num_threads}.data
+	    echo "${iteration_string}${mpi_time_string}" >> ./bench/mpi/${input_name}/p${num_threads}.data
+
+	    echo "${iteration_string} ${omp_time_string:19}, ${mpi_time_string:19}"
+	    omp_time_sum=$(echo ${omp_time_sum} + ${omp_time_string:19} | bc)
+	    mpi_time_sum=$(echo ${mpi_time_sum} + ${mpi_time_string:19} | bc)
+
+	done
+
+	echo "Average execution time (s): $(echo "scale=6; x=${omp_time_sum} / ${iterations}; if(x<1) print 0; x" | bc)" >> ./bench/omp/${input_name}/p${num_threads}.data
+	echo "Average execution time (s): $(echo "scale=6; x=${mpi_time_sum} / ${iterations}; if(x<1) print 0; x" | bc)" >> ./bench/mpi/${input_name}/p${num_threads}.data
+
+	echo "p=$num_threads              omp       mpi"
+	echo "Avg time (s):  $(echo "scale=6; x=${omp_time_sum} / ${iterations}; if(x<1) print 0; x" | bc), $(echo "scale=6; x=${mpi_time_sum} / ${iterations}; if(x<1) print 0; x" | bc)"
+	echo ""
+
 done
 
-echo "Average execution time (s): $(echo "scale=6; x=${serial_time_sum} / ${iterations}; if(x<1) print 0; x" | bc)" >> ./bench/serial/${input_name}.data
-echo "Average execution time (s): $(echo "scale=6; x=${omp_time_sum} / ${iterations}; if(x<1) print 0; x" | bc)" >> ./bench/omp/${input_name}.data
-echo "Average execution time (s): $(echo "scale=6; x=${mpi_time_sum} / ${iterations}; if(x<1) print 0; x" | bc)" >> ./bench/mpi/${input_name}.data
-#echo "Average execution time (s): $(echo "scale=6; ${time_sum} / ${iterations}" | bc)" | tee -a ./bench/omp/${input_name}.data
-echo "                serial     omp       mpi"
-echo "Avg time (s):  $(echo "scale=6; x=${serial_time_sum} / ${iterations}; if(x<1) print 0; x" | bc), $(echo "scale=6; x=${omp_time_sum} / ${iterations}; if(x<1) print 0; x" | bc), $(echo "scale=6; x=${mpi_time_sum} / ${iterations}; if(x<1) print 0; x" | bc)"
 
 rm -f ./.*temp
